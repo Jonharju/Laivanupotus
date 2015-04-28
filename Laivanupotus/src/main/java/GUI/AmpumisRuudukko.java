28308c71f356a911ace1f723fc4c3c8fa16cb852
Jonas Harjunpää <jonas.harjunpaa@helsinki.fi>
2015-04-28 09:54:57 +0300

    pientä hienosäätöä

--- Laivanupotus/src/main/java/GUI/AmpumisRuudukko.java
+++ Laivanupotus/src/main/java/GUI/AmpumisRuudukko.java
@@ -15,6 +15,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+
 public class AmpumisRuudukko extends JPanel{
     private final int koko = 10;
     private Laivasto l;
@@ -89,92 +90,4 @@ public class AmpumisRuudukko extends JPanel{
         }
 
     }
-    
-    //    public void varitaNappulat() {
-//        Pelilauta pelilauta = kayttaja.getPelilauta();
-//        
-//        for (int j = 0; j < koko; j++) {
-//            for (int i = 0; i < koko; i++) {
-//                Ruutu ruutu = pelilauta.haeRuutu(new Sijainti(i, j));
-//                
-//                JButton nappula = haeNappula(ruutu.getSijainti());
-//                
-//                char merkki = haeMerkki(ruutu.getSijainti());
-//                varita(nappula, merkki);
-//            }
-//        }
-//    }
-//        
-//    private void varita(JButton nappula, int merkki) {
-//        
-//        if (merkki == '-1' || merkki == '2') {
-//            nappula.setBackground(Color.WHITE);
-//        }
-//        else if (merkki == '0') {
-//            nappula.setBackground(Color.BLUE);
-//        }
-//        else if (merkki == '3') {
-//            nappula.setBackground(Color.CYAN);
-//        }
-//        else if (merkki == '1') {
-//            nappula.setBackground(Color.RED);
-//        }
-//        else {  // merkki == '4'
-//            nappula.setBackground(Color.BLACK);
-//        }
-//    }
-//    
-//    private char haeMerkki(Sijainti sijainti) {
-//        PelilaudanPiirtaja piirtaja = new PelilaudanPiirtaja(kayttaja);
-//        return piirtaja.palautaSeuraavaMerkki(sijainti);
-//    }
-//    
-//    private JButton haeNappula(Sijainti sijainti) {
-//        JPanel rivi = (JPanel) this.getComponent(sijainti.getY());
-//        return (JButton) rivi.getComponent(sijainti.getX());
-//    }
-//    
-//    public void varitaRuutu(String sijaintiKentta) {
-//        Sijainti sijainti = new SijainninMaarittaja().maaritaSijainti(sijaintiKentta);
-//        varitaNappulat();
-//        varitaRuutu(sijainti);
-//    }
-//    
-//    private void varitaRuutu(Sijainti sijainti) {
-//        JButton nappula = haeNappula(sijainti);
-//        
-//        if (nappula.getBackground() == Color.WHITE) {
-//            nappula.setBackground(Color.PINK);    
-//        }
-//    }
-//    
-//    public void laivanAsettaminen(String sijaintiKentta, String suuntaKentta) {
-//        Laiva laiva = maaritaLaiva(sijaintiKentta, suuntaKentta, kayttaja.getPelilauta().getLaivat().size());
-//        varitaRuudut(laiva);
-//    }
-//    
-//    public Laiva maaritaLaiva(String sijaintiKenttaText, String suuntaKenttaText, int laivoja) {
-//        int laivanKoko = new LaivanKoonMaarittaja().maaritaKoko(laivoja);
-//        Sijainti sijainti = new SijainninMaarittaja().maaritaSijainti(sijaintiKenttaText);
-//        Suunta suunta = Suunta.ALAS;
-//        
-//        if (suuntaKenttaText.equals("OIKEALLE")) {
-//            suunta = Suunta.OIKEALLE;
-//        }
-//        
-//        return new Laiva(sijainti, suunta, laivanKoko);
-//    }
-//    
-//    private void varitaRuudut(Laiva laiva) {
-//        varitaNappulat();
-//        
-//        if (kayttaja.getPelilauta().voidaankoAsettaa(laiva)) {
-//        
-//            for (int i = 0; i < laiva.getKoko(); i++) {
-//                Sijainti sijainti = laiva.haeLaivanOsanSijainti(i);
-//                varitaRuutu(sijainti);
-//            }
-//        }
-//    }
-    
 }
